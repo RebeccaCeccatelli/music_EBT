e85a127d13cdda2cada9387e4fb1b5a0b6af2b89
RebeccaCeccatelli <rceccatelli@student.ethz.ch>
2026-03-18 18:27:02 -0400

    Anticipation folder: imported convert.py code, with integrated change for JordanAI

---
+++ data/mus/symbolic/tokenization/anticipation/__init__.py
@@ -0,0 +1,13 @@
+"""
+
+This folder implements the Anticipation MIDI-to-Token representation. 
+Uses an absolute arrival-time grid with interleaved control/event streams.
+
+Most of the code is from: https://github.com/jthickstun/anticipation
+or https://github.com/lancelotblanchard/anticipation#
+"""
+
+"""
+Utilities for converting to and from Midi data and encoded/tokenized data.
+"""
+
